ea8a59559bdf6e53967e53afa3f11b165b5c106d
dybala <igorndesigne@gmail.com>
2019-05-24 17:06:08 +0200

    jordan:save user info 24-05-20219

--- app/src/main/java/com/example/jolvalre/beworker/entities/Chercheur.java
+++ app/src/main/java/com/example/jolvalre/beworker/entities/Chercheur.java
@@ -1,5 +1,9 @@
 package com.example.jolvalre.beworker.entities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
@@ -26,7 +30,30 @@ public class Chercheur {
     public static String BIRTH_DAY = "birth_day";
     public static String ADRESSE = "adresse";
 
-    public static void load(ChercheurV2 body){
+    public static void load(ChercheurV2 body, Context context){
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        ChercheurV2 chercheur = body;
+
+        editor.putString(Chercheur.ID, String.valueOf(chercheur.getId_chercheur()) );
+        editor.putString(Chercheur.NOM, chercheur.getNom() );
+
+        editor.putString(Chercheur.PRENOM, chercheur.getPrenom() );
+        editor.putString(Chercheur.EMAIL, chercheur.getEmail() );
+
+        editor.putString(Chercheur.PASSWORD, chercheur.getMot_de_passe() );
+
+        editor.putString(Chercheur.GENRE, chercheur.getGenre() );
+        editor.putString(Chercheur.BIRTH_DAY, chercheur.getDate_de_naissance().split("T")[0] );
+
+        editor.putString(Chercheur.DOMAINE, chercheur.getDomaine() );
+        editor.putString(Chercheur.STATUT, chercheur.getStatut() );
+
+        editor.putString(Chercheur.VILLE, chercheur.getVille() );
+        editor.putString(Chercheur.TELEPHONE, String.valueOf(chercheur.getTelephone()) );
+
+        editor.apply();
 
     }
 }
